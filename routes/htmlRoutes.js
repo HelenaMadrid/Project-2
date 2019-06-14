1f7436cdb82ccacf290690f2cace5b0ed414857d
HelenaMadrid <helena.madrid@directrips.com>
2019-06-14 16:05:59 -0500

    autocomplete half

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -20,6 +20,10 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/Helena", function(req, res) {
+    res.render("profesionalexperience");
+  });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
